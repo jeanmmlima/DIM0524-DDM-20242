2850402d3023594b7f5ec75f9393868e72134fd3
jeanmmlima <jeanmariomlima@gmail.com>
2024-10-15 17:53:38 -0300

    ajustes para aula

--- f05_lugares_app/lib/main.dart
+++ f05_lugares_app/lib/main.dart
@@ -1,9 +1,27 @@
 
 import 'package:f05_lugares_app/routes/rotas.dart';
+import 'package:f05_lugares_app/screens/lugares_por_pais.dart';
+import 'package:f05_lugares_app/screens/pais_screen.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp.router(
-    routerConfig: myRouter,
-  ));
+  runApp(
+    MeuApp()
+  );
+    
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/' : (context) => PaisScreen(),
+      '/lugaresPais' : (context) => LugarPorPaisScreen()
+    },
+  );
+  }
 }
