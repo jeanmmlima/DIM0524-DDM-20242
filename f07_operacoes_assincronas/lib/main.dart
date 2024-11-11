db16b2604e3400939acfc21371414c9f73b793ed
jeanmmlima <jeanmariomlima@gmail.com>
2024-11-11 13:48:01 -0300

    operacoes assincronas I para aula

--- f07_operacoes_assincronas/lib/main.dart
+++ f07_operacoes_assincronas/lib/main.dart
@@ -19,14 +19,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   Future<String>? _futureData;
   TestRepository _testRepository = TestRepository();
-
-  @override
-  void initState() {
-    super.initState();
-    _futureData = _testRepository
-        .fetchApiData(); // Chamando a função assíncrona no início do estado
-  }
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -38,23 +30,7 @@ class _MyAppState extends State<MyApp> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: FutureBuilder<String>(
-            future: _futureData, // Future a ser aguardado
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                // Exibe um indicador de carregamento enquanto a operação não foi concluída
-                return CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                // Exibe uma mensagem de erro caso algo dê errado
-                return Text('Erro: ${snapshot.error}',style: TextStyle(fontSize: 20),);
-              } else if (snapshot.hasData) {
-                // Exibe os dados quando o Future for concluído com sucesso
-                return Text(snapshot.data!, style: TextStyle(fontSize: 20),);
-              } else {
-                return Text('Nenhum dado disponível',style: TextStyle(fontSize: 20),);
-              }
-            },
-          ),
+          child: Text("Future aqui"),
         ),
       ),
     );
